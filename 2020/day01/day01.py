743dfeeb06f70fd67bab7ce9e061bad208a452ca
Tim Byrne <sultan@locehilios.com>
2020-12-01 22:48:54 -0600

    Add 2020-01
    I have used itertools to review the sums of all combinations of input numbers.
    And functools.reduce to apply the multiply operator to qualified combinations.
    
    In this way I can solve for pairs, triplets, etc.

---
+++ 2020/day01/day01.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+"""
+https://adventofcode.com/2020/day/1
+"""
+import functools
+import itertools
+import operator
+import aoc
+
+PUZZLE = aoc.Puzzle(day=1, year=2020)
+
+
+def solve(count):
+    """Solve puzzle"""
+    dataset = map(int, PUZZLE.input.splitlines())
+    for nums in itertools.combinations_with_replacement(dataset, count):
+        if sum(nums) == 2020:
+            return functools.reduce(operator.mul, nums)
+    return None
+
+
+if __name__ == "__main__":
+    PUZZLE.report_a(solve(2))
+    PUZZLE.report_b(solve(3))
