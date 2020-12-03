3528441193dfbe191c6e31816b47c0ed4b31051f
Tim Byrne <sultan@locehilios.com>
2020-12-03 09:40:45 -0600

    Use modulus instead of generation
    Thought of this while drifting off to sleep...

--- 2020/day03/day03.py
+++ 2020/day03/day03.py
@@ -3,38 +3,25 @@
 """
 https://adventofcode.com/2020/day/3
 """
-import itertools
 import aoc
 
 PUZZLE = aoc.Puzzle(day=3, year=2020)
 
 
-def generate(row):
-    """Generate infinite terrain"""
-    while True:
-        for char in row:
-            yield char
-
-
-def count_trees(right, down):
+def count_trees(*slopes):
     """Count tress matching slope"""
-    col, trees = 0, 0
-    rows = list(PUZZLE.input.splitlines())
-    for row in rows[down::down]:
-        col += right
-        if list(itertools.islice(generate(row), col + 1))[-1] == '#':
-            trees += 1
-    return trees
-
-
-def solve(slopes):
-    """Solve puzzle"""
-    trees = 1
-    for slope in slopes:
-        trees = trees * count_trees(*slope)
-    return trees
+    answer = 1
+    for right, down in [map(int, (x, y)) for x, y in slopes]:
+        col, trees = 0, 0
+        rows = PUZZLE.input.splitlines()
+        for row in rows[down::down]:
+            col += right
+            if row[col % len(row)] == '#':
+                trees += 1
+        answer = answer * trees
+    return answer
 
 
 if __name__ == "__main__":
-    PUZZLE.report_a(solve([[3, 1]]))
-    PUZZLE.report_b(solve([[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]))
+    PUZZLE.report_a(count_trees('31'))
+    PUZZLE.report_b(count_trees('11', '31', '51', '71', '12'))
