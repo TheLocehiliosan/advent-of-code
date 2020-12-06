15c211df061c1715e7a326e990b70c2547a6bb54
Tim Byrne <sultan@locehilios.com>
2020-12-05 23:29:33 -0600

    Add 2020-06
    Once the input was parsed into records (group size & answers), it was
    easy to process the answers as a set.

---
+++ 2020/day06/day06.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+"""
+https://adventofcode.com/2020/day/6
+"""
+import aoc
+
+PUZZLE = aoc.Puzzle(day=6, year=2020)
+
+
+def answers():
+    """Generate groups of answers"""
+    for group in PUZZLE.input.split('\n\n'):
+        lines = group.splitlines()
+        yield len(lines), ''.join(lines)
+
+
+def solve():
+    """Solve puzzle"""
+    total_a, total_b = 0, 0
+    for people, ans in answers():
+        for letter in set(ans):
+            total_a += 1
+            if ans.count(letter) == people:
+                total_b += 1
+    PUZZLE.report_a(total_a)
+    PUZZLE.report_b(total_b)
+
+
+if __name__ == "__main__":
+    solve()
