6dd9c9c1c478655901c89d4cc1f94c080bd8f9c1
Tim Byrne <sultan@locehilios.com>
2020-12-07 23:26:37 -0600

    Add 2020-08
    For the first part, I stepped through the program, tracking which
    instructions have already been executed with a set.
    
    For the second part, I brute force swapped "nop" & "jmp", and ran each
    program to find then one that completed.

---
+++ 2020/day08.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+"""
+https://adventofcode.com/2020/day/8
+"""
+import aoc
+
+PUZZLE = aoc.Puzzle(day=8, year=2020)
+
+
+def solve(part):
+    """Solve puzzle"""
+    source = PUZZLE.input.splitlines()
+    for index, line in enumerate(source):
+        pgm = source.copy()
+        if part == 'b':
+            line = line.translate(str.maketrans('nojm', 'jmno'))
+        pgm[index] = line
+        acc, counter, seen = 0, 0, set()
+        while True:
+            if counter == len(pgm):
+                return acc
+            if counter in seen:
+                if part == 'a':
+                    return acc
+                break
+            seen.add(counter)
+            inst, param = pgm[counter].split()
+            acc += int(param) if inst == 'acc' else 0
+            counter += int(param) if inst == 'jmp' else 1
+
+
+if __name__ == "__main__":
+    PUZZLE.report_a(solve('a'))
+    PUZZLE.report_b(solve('b'))
